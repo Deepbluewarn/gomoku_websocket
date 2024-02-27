d3046c553f2d32ed275ff49a62bbd6dfc12bb5d4
deepbluewarn <n5lp97@gmail.com>
2024-02-27 13:01:12 +0900

    상수 변수명 수정

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-export const REDIS_SESSION_SETTING = {
-    host: process.env.REDIS_SESSION_HOST || '',
-    port: Number(process.env.REDIS_SESSION_PORT) || 0,
+export const REDIS_SETTING = {
+    host: process.env.REDIS_HOST || '',
+    port: Number(process.env.REDIS_PORT) || 0,
     password: process.env.REDIS_PASSWORD || '',
     db: 0
 }
