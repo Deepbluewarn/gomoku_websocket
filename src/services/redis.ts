151bc396c129984825665330748dfc20c2fc40b9
deepbluewarn <n5lp97@gmail.com>
2024-02-27 13:04:25 +0900

    상수 이름 변경 및 redis 자료구조 변경

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -1,9 +1,23 @@
-import { REDIS_SESSION_SETTING } from "../constants.js";
-import { IUserSession } from "../interfaces/index.js";
+import { REDIS_SETTING } from "../constants.js";
+import { IUserSession } from "../interfaces/socket.io.js";
 import { Redis } from 'ioredis';
 
-const redis = new Redis(REDIS_SESSION_SETTING);
+const redis = new Redis(REDIS_SETTING);
 
 export function setUserSession(userID: string, session: IUserSession) {
-    redis.set(userID, JSON.stringify(session));
+    redis.hset('session', userID, JSON.stringify(session));
+}
+
+/** Room */
+
+export function createRoom(roomID: string, roomName: string, userID: string) {
+    redis.hset('room', roomID, JSON.stringify({ roomName, userID }));
+}
+
+export function joinRoom(roomID: string, userID: string) {
+    redis.hset('room', roomID, JSON.stringify({ userID }));
+}
+
+export async function roomExist(roomID: string) {
+    return (await redis.hexists('room', roomID)) === 1;
 }
